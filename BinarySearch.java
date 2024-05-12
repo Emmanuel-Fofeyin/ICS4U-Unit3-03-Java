b2da420d6f2066bef2c52246643696ca855b8078
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-05-12 00:32:21 -0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -29,7 +29,7 @@ final class BinarySearch {
   /**
   * The number of elements in the array.
   */
-  public static final int ARRAY_SIZE = 250;
+  public static final int ARRAY_SIZE = 150;
 
   /**
   * Function finds the index of a number, using Binary Search recursively.
